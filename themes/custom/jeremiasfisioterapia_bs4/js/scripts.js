76fd142c0b29fff6d7d7b649bce39797021419ab
Rafael Garcia Rea <rafagarciarea@gmail.com>
2022-11-05 10:45:52 +0100

    Creación tema jeremiasfeisioterapia_bs4

---
+++ themes/custom/jeremiasfisioterapia_bs4/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the jeremiasfisioterapia_bs4 theme.
+ */
+
+(function ($, _, Drupal) {
+  Drupal.behaviors.jeremiasfisioterapia_bs4 = {
+    attach() {
+      // jeremiasfisioterapia_bs4 JavaScript behaviors goes here.
+    },
+  };
+})(window.jQuery, window._, window.Drupal);
